08d944350594fba4f04721d2313194b3df134c47
Aman <ghumraaman2@gmail.com>
2024-07-21 16:18:12 +0530

    Initial commit for Login

---
+++ src/pages/TaskDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskDetails = () => {
+  return (
+    <div>TaskDetail</div>
+  )
+}
+
+export default TaskDetails
---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice.js";
+import { apiSlice } from "./slices/apiSlice";
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+
+export default store;
